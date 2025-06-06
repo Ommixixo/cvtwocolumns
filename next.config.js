ca7095a3b41a44fd901d37ca3fa9fd756ef7af66
Sergio Del Castillo <sergio@sinecta.com>
2025-06-06 12:31:32 -0600

    Initial commit with portfolio setup

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/cvtwocolumns',
-    assetPrefix: '/cvtwocolumns/',
     images: {
         unoptimized: true
     }
